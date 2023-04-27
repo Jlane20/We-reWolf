5f4c935ee78ecc859dc154d7b8043a28fbd249bc
Justen Lane <justen.lane@gmail.com>
2023-04-27 19:54:43 -0400

    before pulling don 4/17 8pm

--- src/pages/api/lobby/[gameID].js
+++ src/pages/api/lobby/[gameID].js
@@ -3,7 +3,6 @@ import model from '../../../server/model.js'
 const handler = async (req, res) => {
   const { gameID } = req.query
   if(req.method === 'PUT'){
-
     model.updateLobby(gameID, req.body.user)
     .then(data => (
       res.status(201).send(data)
@@ -20,8 +19,11 @@ const handler = async (req, res) => {
       res.status(401).send(err)
     ))
   } else if (req.method === 'GET') {
+
     model.getLobby(gameID)
       .then(data => {res.status(200).send(data)})
       .catch(err => {res.status(401).send(err)})
   }
 }
+
+export default handler
